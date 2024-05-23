32e3a88ecc07b9b863feeac7c0939fec23f1228c
shuhreeezy <shuhrat.babadjanov.123@gmail.com>
2024-05-23 07:28:59 +0000

    added code to # Screen Dimensions

--- flappy-bird-game/flappy_bird.py
+++ flappy-bird-game/flappy_bird.py
@@ -10,6 +10,8 @@ SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy Bird Clone")
+CLOCK = pygame.time.Clock()
+FPS = 30
 
 # Colors
 WHITE = (255, 255, 255)
